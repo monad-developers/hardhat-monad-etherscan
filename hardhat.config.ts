d3b0bae54f01668284d6917f989d10210e73a828
port <108868128+portdeveloper@users.noreply.github.com>
2025-04-30 14:32:43 +0300

    Add support for etherscan

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,31 +5,36 @@ import "dotenv/config";
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
+const RPC_URL = process.env.RPC_URL || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+const API_URL = process.env.API_URL || "";
+const BROWSER_URL = process.env.BROWSER_URL || "";
+
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.28",
-    settings: {
-      metadata: {
-        bytecodeHash: "none", // disable ipfs
-        useLiteralContent: true, // use source code
-      },
-    },
   },
   networks: {
     monadTestnet: {
-      url: "https://testnet-rpc.monad.xyz",
+      url: RPC_URL,
       accounts: [PRIVATE_KEY],
       chainId: 10143,
     },
   },
-  sourcify: {
-    enabled: true,
-    apiUrl: "https://sourcify-api-monad.blockvision.org",
-    browserUrl: "https://testnet.monadexplorer.com",
-  },
-  // To avoid errors from Etherscan
   etherscan: {
-    enabled: false,
+    apiKey: {
+      monadTestnet: ETHERSCAN_API_KEY,
+    },
+    customChains: [
+      {
+        network: "monadTestnet",
+        chainId: 10143,
+        urls: {
+          apiURL: API_URL,
+          browserURL: BROWSER_URL,
+        },
+      },
+    ],
   },
 };
 
